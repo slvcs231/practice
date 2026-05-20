daa0d0aecbf2e791ff5f73c64120a887631108e3
Reddeiah <reddeiah18g@gmail.com>
2026-05-20 15:52:35 +0530

    devops project

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Reddi:: Hi Likith, how are you?"
+echo "Likith:: Hi Reddi, i am fine. How are you?"
+echo "Reddi:: What are you learning recently?"
+echo "Likith:: I am learning shell scripting"
