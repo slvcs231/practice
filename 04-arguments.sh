20fcd13d876efeec70cec603a9ed83e058f678bc
Reddeiah <reddeiah18g@gmail.com>
2026-05-21 09:31:26 +0530

    variable added

---
+++ 04-arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSION1= $1
+PERSION2= $2
+echo "$PERSION1:: Hi Likith, how are you?"
+echo "$PERSION2:: Hi $PERSION1, i am fine. How are you?"
+echo "$PERSION1:: What are you learning recently?$PERSION2"
+echo "$PERSION2:: I am learning shell scripting $PERSION1"
