9d6c35df1d0ed8a59f0ce3692b0e0d10130cb2e0
Reddeiah <reddeiah18g@gmail.com>
2026-05-22 10:03:49 +0530

    added file

---
+++ 05-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "enter your Pin number"
+read PIN_NUMBER;
+
+echo "Pin entered is : $PIN_NUMBER"
---
+++ 08-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+NAME=Devops
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo $SUM
