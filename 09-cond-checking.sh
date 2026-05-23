94aee1765fa61311277d7bc0e140f97259ea5e2e
Reddeiah <reddeiah18g@gmail.com>
2026-05-23 20:57:28 +0530

    new file

--- 09-cond-checking.sh
+++ 09-cond-checking.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "Given Number $NUMBER is less than 10"
 elif [ $NUMBER -eq 10 ]; then
-    echo "Given Number $NUMBER is equal to q0"
+    echo "Given Number $NUMBER is equal to 10"
 else
     echo "Given Number $NUMBER is greater than 10"
 fi
