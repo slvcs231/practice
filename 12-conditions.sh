6cb6833e1e06d7ce807c2c45b3afb171df5eeb62
Reddeiah <reddeiah18g@gmail.com>
2026-05-23 21:41:30 +0530

    new file

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -4,13 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "ERROR:: Please execute script with root previlige"
-   exit
+   exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Installing MYSQL is failure"
+    exit 1
 else
     echo "Mysql installation is success"
 fi
